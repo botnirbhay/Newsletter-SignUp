1d5217a9401e4c77f049602094414237643af9bb
Nirbhay <hanjuranirbhay@gmail.com>
2022-06-20 18:02:51 +0530

    added action to form

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const request=require("request");
+const { application } = require("express");
 const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res)
+{
+    // the away to connect html pages to server is using send file
+    res.sendFile(__dirname+"/signup.html")
+});
+app.post("/",function(req,res)
+{
+    var firstName=req.body.fname;
+    var lastName=req.body.lname;
+    var email=req.body.email;
+    console.log(firstName,lastName,email);
 
+});
 
 
 
